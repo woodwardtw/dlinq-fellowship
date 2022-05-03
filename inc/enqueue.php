35ef6c57a0a75607f6bbe2062b2445fd324b5d70
woodwardtw <twwoodward@me.com>
2022-05-03 10:48:03 -0400

    aos tie in

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -29,11 +29,14 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . $theme_styles );
 		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . $theme_styles, array(), $css_version );
+		wp_enqueue_style( 'aos-styles', get_template_directory_uri() . '/css/aos.css', array(), $css_version );
 
 		wp_enqueue_script( 'jquery' );
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . $theme_scripts );
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . $theme_scripts, array(), $js_version, true );
+		wp_enqueue_script( 'aos-scripts', get_template_directory_uri() . '/js/aos.js', array(), $js_version, true );
+		wp_enqueue_script( 'fellowship-scripts', get_template_directory_uri() . '/js/main.js', array(), $js_version, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
