278014719936ed8a515603145f7bf1bc5c02ad3f
woodwardtw <twwoodward@me.com>
2022-05-03 10:08:13 -0400

    initial css and move of main js etc. for aos

---
+++ js/main.js
@@ -0,0 +1,32 @@
+AOS.init({
+	mirror: false, 
+})
+
+// const watchDivs = document.querySelectorAll('.watch-me');
+
+// watchDivs.forEach((watchDiv) => {
+//   watchDiv.addEventListener('click', () => {
+//     console.log("forEach worked");
+//   });
+// });
+
+
+// document.addEventListener('scroll', function(e) {
+// const container = document.querySelector('#projects')
+// var lastScroll = container.getBoundingClientRect().top;
+// //container.scrollLeft
+
+//     window.requestAnimationFrame(function() {
+//     	addAttention(container, lastScroll)
+//     });
+
+// });
+
+// function addAttention(theDiv, lastScroll){
+//          if(lastScroll < 120){
+//          	theDiv.classList.add('highlight')
+//          }
+//           if(lastScroll < -200){
+//          	theDiv.classList.remove('highlight')
+//          }
+// }
