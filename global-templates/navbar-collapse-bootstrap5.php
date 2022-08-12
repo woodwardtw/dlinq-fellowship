260723db132009aeaa6438a55fb8856415120f22
woodwardtw <twwoodward@me.com>
2022-08-12 09:18:53 -0400

    real menu integration
    improved menu css

--- global-templates/navbar-collapse-bootstrap5.php
+++ global-templates/navbar-collapse-bootstrap5.php
@@ -13,8 +13,22 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <nav>
     		<div class="menu-links">
-	    		<a href="https://dlinq.middcreate.net">DLINQ</a>
+    			<?php
+					wp_nav_menu(
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => '',
+							'container_id'    => '',
+							'menu_class'      => 'fellows',
+							'fallback_cb'     => '',
+							'menu_id'         => 'fellows-menu',
+							'depth'           => 2,
+							'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+						)
+					);
+					?>
+	    		<!-- <a href="https://dlinq.middcreate.net">DLINQ</a>
 	    		<a href="mailto:dlinq@middcreate.net?subject=Digital%20Teaching%20and%20Learning%20Fellowship">Contact Us</a>
-	    		<a href="#apply">Apply</a>
+	    		<a href="#apply">Apply</a> -->
 	    	</div>
 </nav>
